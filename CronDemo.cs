62d8569547102c1d7622cc56fddaee21b447930f
P Gerard <paulfgerard@gmail.com>
2021-04-21 19:39:50 +0100

    increase API polling rate to every 10s

--- CronDemo.cs
+++ CronDemo.cs
@@ -9,7 +9,7 @@ namespace CronFunctionDemo
     public static class CronDemo
     {
         [FunctionName("CronDemo")]
-        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
